6642b0b50bb6ec793706c99db5c70b5b9477dafd
dracoalv <dracoalv1@gmail.com>
2022-05-02 17:18:17 -0300

    desktop version added

---
+++ js/swiper.js
@@ -0,0 +1,34 @@
+import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  slidesPerView: 1,
+  spaceBetween: 32,
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
